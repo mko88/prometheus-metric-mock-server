2e5eb31f40dfab67d4e8ce745dbbd456cf07fd94
mko88 <mihail.kolev.88@gmail.com>
2024-06-05 18:51:13 +0300

    Add code comments

--- src/main.py
+++ src/main.py
@@ -3,19 +3,25 @@ from metric import Metric
 from store import Store
 import json
 
+# Store all metrics into the store object
 STORE = Store()
+# The flask application
 app = Flask(__name__)
 
 
 @app.route('/metrics', methods=['GET'])
 def get_metrics():
+    """Returns all available metrics."""
+    # Make the response from store content
     response = make_response(STORE.export(), 200)
+    # We need it as simple text
     response.mimetype = 'text/plain'
     return response
 
 
 @app.route('/add', methods=['POST'])
 def add_metrics():
+    """Add metric request."""
     request_data = json.loads(request.data)
     for metric in request_data:
         STORE.upsert(Metric(name=metric['name'],
@@ -30,6 +36,7 @@ def add_metrics():
 
 @app.route('/remove', methods=['POST'])
 def remove_metrics():
+    """Removes a metric by its readable unique id string - name{label=\"value\"}"""
     request_data = json.loads(request.data)
     for name in request_data:
         STORE.remove(unique_id_str=name)
@@ -37,6 +44,5 @@ def remove_metrics():
 
 
 if __name__ == '__main__':
+    """Run the application"""
     app.run(host='0.0.0.0')
-
-
--- src/store.py
+++ src/store.py
@@ -3,20 +3,27 @@ from metric import Metric, get_metric_uuid, default_metric
 
 class Store:
     def __init__(self):
+        """The metric store, initially contains a single default metric provided by the mock."""
         self.metrics = {}
         self.upsert(default_metric())
 
     def export(self):
+        """Export the store content to string that can be scraped by Prometheus."""
         output = ''
+        # Gather unique metric names.
         metric_names = set(metric.name for metric in self.metrics.values())
         for metric_name in metric_names:
+            # Gather all metrics for name.
             metrics = [metric for metric in self.metrics.values() if metric.name == metric_name]
+            # Append prometheus header from first metric. Others will be ignored.
             output += metrics[0].prometheus_header()
             for metric in metrics:
+                # Append all metrics below the header.
                 output += metric.prometheus_value()
         return output
 
     def upsert(self, metric: Metric):
+        """Insert or update metric to the store."""
         if metric.unique_id in self.metrics.keys():
             print(f'Updating metric: {metric.unique_id_str} {metric.value}')
         else:
@@ -24,6 +31,7 @@ class Store:
         self.metrics.update({metric.unique_id: metric})
 
     def remove(self, unique_id_str: str):
+        """Remove metric from the store by unique id string"""
         unique_id = get_metric_uuid(unique_id_str)
         if unique_id in self.metrics.keys():
             self.metrics.pop(unique_id)
